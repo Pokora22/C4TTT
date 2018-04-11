6d1c9f63c6f47ca99d9abd6900f7543bc57ea206
Jakub <jakubster9@gmail.com>
2018-04-11 13:21:06 +0100

    tokenPlacer method added

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,21 @@
 public class TicTacToe extends Board {
+    private int posPlacerX;
+    private int posPlacerY;
+
+    void tokenPlacer(String input) {
+
+        ////// (COLUMN, ROW)
+        if(input.contains("A"))
+            this.posPlacerY=0;
+        if(input.contains("B"))
+            this.posPlacerY=1;
+        if(input.contains("C"))
+            this.posPlacerY=2;
+        if(input.contains("1"))
+            this.posPlacerX=0;
+        if(input.contains("2"))
+            this.posPlacerX=1;
+        if(input.contains("3"))
+            this.posPlacerX=2;
+    }
 }
